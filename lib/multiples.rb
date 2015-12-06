86ea910f6040ebbbc842883007d6e2ee7f73dad5
hscoder <hebermisael@yahoo.com>
2015-12-06 22:37:17 +0000

    nitrous-backup at Sun Dec  6 22:37:17 UTC 2015

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+#def sum35(n)
+#  (1...n).select{|i|i%3==0 or i%5==0}.inject(:+)
+#end
+#puts sum35(1000)
+
+def multiples
+  (1..999).select do |i|
+    i % 3 == 0 || i % 5 == 0
+  end
+end
+
+def sumMultiples(multiples)
+  total = multiples.inject(&:+)
+end
